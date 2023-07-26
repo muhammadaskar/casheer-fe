f4b6585f248553241ee00e2ac028febb2afeb85e
Muhammad Vikri <muhammadvikrii99@gmail.com>
2023-07-26 22:02:10 +0700

    fix: dark mode useEffect

--- src/hooks/use-darkmode.ts
+++ src/hooks/use-darkmode.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useDarkMode = () => {
   const [darkMode, setDarkMode] = useState(
@@ -6,49 +6,21 @@ const useDarkMode = () => {
   );
   const htmlClasses = document.querySelector('html');
 
-  //   const toggleDarkMode = () => {
-  //     darkMode === '' ? setDarkMode('dark') : setDarkMode('');
-  //   };
+  const toggleDarkMode = () => {
+    darkMode === 'light' ? setDarkMode('dark') : setDarkMode('light');
+  };
 
-  const toggleDarkMode = useCallback(() => {
-    if (darkMode === 'light') {
-      setDarkMode('dark');
+  useEffect(() => {
+    if (darkMode === 'dark') {
       document.documentElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
       htmlClasses?.classList.add('dark');
     } else {
-      setDarkMode('light');
       document.documentElement.classList.remove('dark');
       localStorage.setItem('theme', 'light');
       htmlClasses?.classList.remove('dark');
     }
   }, [darkMode, htmlClasses]);
-
-  const onWindowMatch = () => {
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  };
-
-  onWindowMatch();
-
-  //   useEffect(() => {
-  //     if (darkMode === 'dark') {
-  //       document.documentElement.classList.add('dark');
-  //       localStorage.setItem('theme', 'dark');
-  //       htmlClasses?.classList.add('dark');
-  //     } else {
-  //       document.documentElement.classList.remove('dark');
-  //       localStorage.setItem('theme', 'light');
-  //       htmlClasses?.classList.remove('dark');
-  //     }
-  //   }, [darkMode, htmlClasses]);
 
   return {
     darkMode,
