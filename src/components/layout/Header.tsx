d9122f8380c7523b658c10b22cac91039d432c51
Muhammad Vikri <muhammadvikrii99@gmail.com>
2023-07-26 22:14:27 +0700

    fix: icon dark mode

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -68,7 +68,7 @@ const Header = () => {
           className="h-8 w-8"
           onClick={toggleDarkMode}
         >
-          {darkMode === 'dark' ? (
+          {darkMode === 'light' ? (
             <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           ) : (
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
