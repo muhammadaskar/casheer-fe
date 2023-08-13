57ded384d93a75f5dd741c74a48bee01269b4431
Muhammad Vikri <muhammadvikrii99@gmail.com>
2023-08-14 01:36:01 +0700

    fix: table responsive

--- src/pages/management/product/index.tsx
+++ src/pages/management/product/index.tsx
@@ -14,14 +14,18 @@ const Product = () => {
 
   return (
     <main className="px-2 md:px-5 mx-auto py-2 md:py-5 space-y-3">
-      <h1 className="font-semibold tracking-tight text-2xl">Product</h1>
+      <h1 className="hidden md:block font-semibold tracking-tight text-2xl">
+        Product
+      </h1>
       <h1 className="font-semibold tracking-tight text-2xl block md:hidden">
         {value.charAt(0).toUpperCase() + value.slice(1)}
       </h1>
       <p className="text-sm text-muted-foreground">Lorem ipsum dolor amet.</p>
       <Separator className="my-4 hidden md:block" />
 
-      <DataTable columns={columns} data={data?.data} />
+      <div className="hidden md:block">
+        <DataTable columns={columns} data={data?.data} />
+      </div>
 
       <Tabs className="block md:hidden space-y-3" defaultValue="product">
         <TabsList className="w-full grid grid-cols-4">
